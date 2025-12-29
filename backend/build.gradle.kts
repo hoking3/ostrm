e03e52d3c54f4e13e40947aeb1e4887d0492e148
Hienao <shiwentao666@gmail.com>
2025-12-30 00:30:25 +0800

    feat: 添加日志管理页面及后端服务，支持日志的实时查看、筛选和下载功能。

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.apache.commons:commons-lang3:3.17.0")
     implementation("org.apache.commons:commons-collections4:4.4")
+    implementation("commons-io:commons-io:2.15.1")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
     implementation("com.auth0:java-jwt:4.4.0")
     implementation("org.flywaydb:flyway-core:$flywayVersion")
